fc6fe63fe4307bebc087bcaafb41658a86f57b42
Benjamin <tankerkzheng1@gmail.com>
2024-03-29 18:46:47 +0800

    feat: write function done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,44 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { arbitrumSepolia } from "wagmi/chains";
+import { walletConnect } from "wagmi/connectors";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "fc8acddf0c859465327faa85a2a3edac";
+
+const chains = [arbitrumSepolia] as const;
+
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata: {
+    name: "arbiworkshop",
+    description: "APUBCC Bootcamp",
+    url: "",
+    icons: [],
+  },
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
